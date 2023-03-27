54181091f5eec5d485ae307fa67113db219d0386
Lucas Martins <96502027+lmartins12@users.noreply.github.com>
2023-03-26 23:40:16 -0300

    Develop (#2)
    * Criação de homepage e novos componente
    
    * Construção do componente about-me
    
    * padronização do título dos componentes
    
    * Adição de projetos e efeitos de transição
    
    * atualização componente home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+
+export class HomeComponent implements OnInit, OnDestroy {
 
   text = "Olá, seja bem vindo!";
   textComplete = "";
   delay = 100;
   i = 0;
 
-  ngOnInit(): void {
+  private audio!: HTMLAudioElement;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit() {
+    this.audio = this.renderer.createElement('audio');
+    this.audio.src = 'assets/portfolioAmbientSound.MP3';
+    this.audio.load();
+    this.renderer.appendChild(document.body, this.audio);
+
+    this.audio.play();
     this.digitar();
+  }
+
+  ngOnDestroy() {
+    this.audio.pause();
   }
 
   digitar() {
