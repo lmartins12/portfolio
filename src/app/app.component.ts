c9792757794dd3eea40c87f6bc349747994c3088
Lucas Martins <lucas.martins559@outlook.com>
2023-04-02 01:26:05 -0300

    animações no scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  ngOnInit(): void {
+      AOS.init({
+        delay: 200,
+      });
+  }
 }
